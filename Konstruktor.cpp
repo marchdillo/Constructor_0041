cd29686783ad3861ef6a47f7a2014c89762e6565
marchdillo <kevinno.limiy@gmail.com>
2025-05-17 18:18:02 +0700

    Commit 9

--- Konstruktor.cpp
+++ Konstruktor.cpp
@@ -44,3 +44,18 @@ void mahasiswa::cetak()
          << " Nim = " << nim << endl;
     cout << " Nama = " << nama << endl;
 }
+
+int main()
+{
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Indra");
+    mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
